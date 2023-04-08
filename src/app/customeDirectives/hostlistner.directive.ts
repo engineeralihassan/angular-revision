d0696e4d5bf444c76a051306eb45147dd2e2f1fc
Ali Hassan <engineralihassan@gmail.com>
2023-04-07 17:37:50 -0700

    All Till custome attribute directives

--- src/app/customeDirectives/hostlistner.directive.ts
+++ src/app/customeDirectives/hostlistner.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHostlistner]'
@@ -6,19 +6,21 @@ import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@an
 export class HostlistnerDirective {
 
   constructor(private element: ElementRef, private rendrer: Renderer2) {
-
-
   }
-  @HostBinding('style.color') color = 'blue'
+  @Input() defualtColor: string = 'gray';
+  @Input() hoverColor: string = 'brown';
+
+
+  @HostBinding('style.color') color = this.defualtColor;
   @HostListener('mouseenter') onmouse() {
     this.rendrer.setStyle(this.element.nativeElement, 'background-color', "red");
-    this.color = "blue";
+    this.color = this.defualtColor;
     this.rendrer.setStyle(this.element.nativeElement, 'color', this.color);
     this.rendrer.setStyle(this.element.nativeElement, 'padding', '20px');
     this.rendrer.setStyle(this.element.nativeElement, 'transition', '0.6s');
   }
   @HostListener('mouseleave') offmouse() {
-    this.color = 'pink';
+    this.color = this.hoverColor;
     this.rendrer.setStyle(this.element.nativeElement, 'background-color', "green")
     this.rendrer.setStyle(this.element.nativeElement, 'color', this.color);
     this.rendrer.setStyle(this.element.nativeElement, 'padding', '10px');
